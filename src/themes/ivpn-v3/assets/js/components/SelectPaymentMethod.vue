ca8ead6f7a66515f1125b93a48ef051502435254
Fedir Nepyyvoda <fedir@ivpn.net>
2020-12-07 18:50:03 +0200

    Moves "IVPN Gift Card" payments under "additional payments" menu.

--- src/themes/ivpn-v3/assets/js/components/SelectPaymentMethod.vue
+++ src/themes/ivpn-v3/assets/js/components/SelectPaymentMethod.vue
@@ -5,11 +5,16 @@
                 <div class="plan-details" v-if="account.is_new">
                     <div class="plan-name">{{ account.product.name }}</div>
                     <div class="plan-change">
-                        <router-link :to="{name: 'prices'}">Change product</router-link>
+                        <router-link :to="{ name: 'prices' }"
+                            >Change product</router-link
+                        >
                     </div>
                 </div>
 
-                <select-billing-cycle :options="account.product.prices" v-model="price"></select-billing-cycle>
+                <select-billing-cycle
+                    :options="account.product.prices"
+                    v-model="price"
+                ></select-billing-cycle>
             </div>
             <div class="gap"></div>
         </div>
@@ -17,38 +22,56 @@
             <router-link
                 tag="button"
                 class="btn btn-solid pay-button"
-                :to="{ name: 'add-funds-cc', params: { price: price.id }}"
+                :to="{ name: 'add-funds-cc', params: { price: price.id } }"
             >
-                <div class="credit-card-icon"></div>Credit Card
+                <div class="credit-card-icon"></div>
+                Credit Card
             </router-link>
             <router-link
                 tag="button"
                 class="btn btn-solid pay-button"
-                :to="{ name: 'add-funds-paypal', params: { price: price.id }}"
+                :to="{ name: 'add-funds-paypal', params: { price: price.id } }"
             >
-                <div class="paypal-icon"></div>PayPal
+                <div class="paypal-icon"></div>
+                PayPal
             </router-link>
             <router-link
                 tag="button"
                 class="btn btn-solid pay-button"
-                :to="{ name: 'add-funds-bitcoin', params: { price: price.id }}"
+                :to="{ name: 'add-funds-bitcoin', params: { price: price.id } }"
             >
-                <div class="bitcoin-icon"></div>Bitcoin
+                <div class="bitcoin-icon"></div>
+                Bitcoin
             </router-link>
             <router-link
                 tag="button"
                 class="btn btn-solid pay-button"
-                :to="{ name: 'add-funds-cash', params: { price: price.id }}"
+                :to="{ name: 'add-funds-cash', params: { price: price.id } }"
             >
-                <div class="cash-icon"></div>Cash
-            </router-link>
-            <router-link
+                <div class="cash-icon"></div>
+                Cash
+            </router-link>            
+        </div>
+        <div class="pay-buttons">
+            <div class="more-methods">
+                <a @click.prevent="toggleMoreOptions()" v-if="!more" href='#'
+                    >More payment options</a
+                >
+                <a @click.prevent="toggleMoreOptions()" v-else href='#'
+                    >Hide additional payment options</a
+                >
+            </div>        
+            <router-link v-if="more"
                 tag="button"
                 class="btn btn-solid pay-button"
-                :to="{ name: 'add-funds-giftcard', params: { price: price.id }}"
+                :to="{
+                    name: 'add-funds-giftcard',
+                    params: { price: price.id },
+                }"
             >
-                <div class="giftcard-icon"></div>Gift Card
-            </router-link>
+                <div class="giftcard-icon"></div>
+                IVPN Gift Card
+            </router-link>    
         </div>
     </div>
 </template>
@@ -65,18 +88,27 @@ export default {
     data() {
         return {
             price: "",
+            more: false,
         };
     },
-    created() {    
+    created() {
         this.price = this.$store.state.payments.selectedPrice;
-                
-        if (this.price == null || !this.account.product.prices.includes(this.price)) {
+
+        if (
+            this.price == null ||
+            !this.account.product.prices.includes(this.price)
+        ) {
             this.price = this.account.product.prices[2];
         }
     },
     watch: {
         price: function () {
             this.$store.commit("payments/setSelectedPrice", this.price);
+        },
+    },
+    methods: {
+        toggleMoreOptions() {
+            this.more = !this.more;
         },
     },
 };
@@ -137,5 +169,10 @@ export default {
         margin: 20px 24px 0px 0px;
         font-size: 18px;
     }
+}
+
+.more-methods {
+    margin-top: 2em;
+    text-align: right;
 }
 </style>
