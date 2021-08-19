76196ba6de122c86410f02d953dc10b2c14f54d2
Juraj Hilje <juraj@ivpn.net>
2021-08-19 10:59:06 +0200

    Update ServerList.vue

--- src/themes/ivpn-v3/assets/js/components/ServerList.vue
+++ src/themes/ivpn-v3/assets/js/components/ServerList.vue
@@ -2,48 +2,44 @@
     <table>
         <thead>
             <tr>
+                <th class="hostname">HOSTNAME</th>
                 <th class="location">LOCATION</th>
-                <th class="hostnames">HOSTNAMES</th>
                 <th class="load">LOAD</th>
                 <th class="provider">PROVIDER</th>
+                <th class="port">MULTIHOP PORT</th>
                 <th class="wg_public_key">WIREGUARD PUBLIC KEY</th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="server in sortedList">
+                <td class="hostname">
+                    <em>Hostnames</em>
+                    <ul>
+                        <li>
+                            <i class="status" :class="{ 'status--active': server.is_active }">
+                            </i>{{ server.gateway }}
+                        </li>
+                    </ul>
+                </td>
                 <td class="location">
                     <div class="location__data">
                         <img :src="'/images-static/flags/' + server.country_code.toLowerCase() + '.svg'" :alt="server.country_code.toUpperCase()">
-                        <span> {{ server.country }}, {{ server.city }} </span>
+                        <span> {{ server.city }}, {{ server.country_code }} </span>
                     </div>
-                </td>
-                <td class="hostnames">
-                    <em>Hostnames</em>
-                    <ul>
-                        <li
-                            v-for="(host, protocol) in server.hostnames"
-                            :key="protocol"
-                        >
-                            <i
-                                class="status"
-                                :class="{ 'status--active': server.is_active }"
-                            ></i
-                            >{{ host }}
-                            <span class="badge badge--light spacing">{{
-                                protocol
-                            }}</span>
-                        </li>
-                    </ul>
                 </td>
                 <td class="load">
                     <em>Load</em>
                     {{ server.load }}%
                 </td>
-                <td>
+                <td class="provider">
                     <em>Provider</em>
                     {{ server.isp }}
                 </td>
-                <td>
+                <td class="port">
+                    <em>MultiHop port</em>
+                    {{ server.multihop_port }}
+                </td>
+                <td class="wg_public_key">
                     <em>WireGuard key</em>
                     {{ server.wg_public_key }}
                 </td>
