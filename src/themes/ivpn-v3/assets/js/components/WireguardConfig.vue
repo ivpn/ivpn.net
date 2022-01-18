f38e7ebba4a9e899d8859790b73649afc25b1b13
Juraj Hilje <juraj@ivpn.net>
2022-01-18 17:12:32 +0100

    Create WireguardConfig.vue

---
+++ src/themes/ivpn-v3/assets/js/components/WireguardConfig.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="vpn-configuration">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    mounted() {
+        
+    },
+    methods: {
+
+    },
+    components: {
+        
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/themes/ivpn-v3/assets/js/wireguard-config.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+
+import WireguardConfig from './components/WireguardConfig';
+
+new Vue({
+    el: "#wireguard-config",
+    components: {
+        WireguardConfig,
+    }
+});
