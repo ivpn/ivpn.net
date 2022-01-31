4a5fb02bf40f1f00166a461e77e4f4366aed6f7d
Juraj Hilje <juraj@ivpn.net>
2022-01-31 15:37:10 +0100

    Update Wireguard.vue and WireguardConfig.vue template

--- src/themes/ivpn-v3/assets/js/views/Account/WireguardConfig.vue
+++ src/themes/ivpn-v3/assets/js/views/Account/WireguardConfig.vue
@@ -13,122 +13,129 @@
             </section>
         </div>
         <div v-if="account.is_active">
-            <h2>WireGuard Configuration</h2>
-            <h3>1. Select platform</h3>
-            <div class="apps-block">
-                <div class="apps-buttons">
-                    <a @click="selectPlatform" data-platform="windows" v-bind:class="{ active: query.platform == 'windows' }" href="">
-                        <icon-windows />Windows
-                    </a>
-                    <a @click="selectPlatform" data-platform="macos" v-bind:class="{ active: query.platform == 'macos' }" href="">
-                        <icon-macos />macOS
-                    </a>
-                    <a @click="selectPlatform" data-platform="linux" v-bind:class="{ active: query.platform == 'linux' }" href="">
-                        <icon-linux />Linux
-                    </a>
-                    <a @click="selectPlatform" data-platform="ios" v-bind:class="{ active: query.platform == 'ios' }" href="">
-                        <icon-ios />iOS
-                    </a>
-                    <a @click="selectPlatform" data-platform="android" v-bind:class="{ active: query.platform == 'android' }" href="">
-                        <icon-android />Android
-                    </a>
+            <div class="back-link">
+                <router-link :to="{ name: 'account/wireguard' }">
+                    <span class="icon-back"></span>Back to WireGuard Settings
+                </router-link>
+            </div>
+            <section>
+                <h2>WireGuard Configuration</h2>
+                <h3>1. Select platform</h3>
+                <div class="apps-block">
+                    <div class="apps-buttons">
+                        <a @click="selectPlatform" data-platform="windows" v-bind:class="{ active: query.platform == 'windows' }" href="">
+                            <icon-windows />Windows
+                        </a>
+                        <a @click="selectPlatform" data-platform="macos" v-bind:class="{ active: query.platform == 'macos' }" href="">
+                            <icon-macos />macOS
+                        </a>
+                        <a @click="selectPlatform" data-platform="linux" v-bind:class="{ active: query.platform == 'linux' }" href="">
+                            <icon-linux />Linux
+                        </a>
+                        <a @click="selectPlatform" data-platform="ios" v-bind:class="{ active: query.platform == 'ios' }" href="">
+                            <icon-ios />iOS
+                        </a>
+                        <a @click="selectPlatform" data-platform="android" v-bind:class="{ active: query.platform == 'android' }" href="">
+                            <icon-android />Android
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <h3>2. Generate WireGuard key</h3>
-            <p v-if="!wgInterface.publicKey">
-                <a class="btn btn-border" href="" @click.prevent="generateKey()">Generate key</a>
-            </p>
-            <p v-if="wgInterface.publicKey">
-                <strong>Public key:</strong><br>
-                {{ wgInterface.publicKey }}
-            </p>
-            <h3>3. Select one or multiple exit servers</h3>
-            <div class="select">
-                <select @change="selectExitCountry($event)">
-                    <option value="">All countries</option>
-                    <option v-for="server in filteredServers" :value="server.country_code" :key="server.country_code">{{ server.country }}</option>
-                </select>
-                <i></i>
-            </div>
-            <div class="select" v-bind:class="{ disabled: validation.exitCity }">
-                <select :disabled="validation.exitCity" @change="selectExitCity($event)">
-                    <option value="">All cities</option>
-                    <option v-for="city in exitCities" :value="city" :key="city">{{ city }}</option>
-                </select>
-                <i></i>
-            </div>
-            <div class="select" v-bind:class="{ disabled: validation.exitServer }">
-                <select :disabled="validation.exitServer" @change="selectExitServer($event)">
-                    <option value="">All servers</option>
-                    <option v-for="server in exitServers" :value="server.gateway + '_' + server.multihop_port" :key="server.gateway">{{ server.gateway }}</option>
-                </select>
-                <i></i>
-            </div>
-            <h3>4. Configuration</h3>
-            <h4>Multihop</h4>
-            <div class="checkbox" v-bind:class="{ disabled: validation.multihop }">
-                <div>
-                    <input type="checkbox" id="multihop" :disabled="validation.multihop" @change="toggleMultihop($event)">
-                    <label for="multihop">Enable</label>
-                </div>
-            </div>
-            <div v-if="multihop">
-                <h4>Select entry server location</h4>
-                <div class="select" v-bind:class="{ disabled: validation.multihop }">
-                    <select :disabled="validation.multihop" @change="selectEntryCountry($event)">
-                        <option value="">Select country</option>
-                        <option v-for="server in multihopServers" :value="server.country_code" :key="server.country_code">{{ server.country }}</option>
-                    </select>
-                    <i></i>
-                </div>
-                <div class="select" v-bind:class="{ disabled: validation.entryCity || validation.multihop }">
-                    <select :disabled="validation.entryCity || validation.multihop" @change="selectEntryCity($event)">
-                        <option value="">Select city</option>
-                        <option v-for="city in entryCities" :value="city" :key="city">{{ city }}</option>
-                    </select>
-                    <i></i>
-                </div>
-                <div class="select" v-bind:class="{ disabled: validation.entryServer || validation.multihop }">
-                    <select :disabled="validation.entryServer || validation.multihop" @change="selectEntryServer($event)">
-                        <option value="">Select server</option>
-                        <option v-for="server in entryServers" :value="server.gateway" :key="server.gateway">{{ server.gateway }}</option>
-                    </select>
-                    <i></i>
-                </div>
-            </div>
-            <div v-if="!multihop">
-                <h4>Port</h4>
+                <h3>2. Generate WireGuard key</h3>
+                <p v-if="!wgInterface.publicKey">
+                    <a class="btn btn-border" href="" @click.prevent="generateKey()">Generate key</a>
+                </p>
+                <p v-if="wgInterface.publicKey">
+                    <strong>Public key:</strong><br>
+                    {{ wgInterface.publicKey }}
+                </p>
+                <h3>3. Select one or multiple exit servers</h3>
                 <div class="select">
-                    <select @change="selectPort($event)">
-                        <option value="2049" :selected="(query.port == 2049) || !query.port">2049</option>
-                        <option value="2050" :selected="(query.port == 2050)">2050</option>
-                        <option value="53" :selected="(query.port == 53)">53</option>
-                        <option value="1194" :selected="(query.port == 1194)">1194</option>
-                        <option value="30587" :selected="(query.port == 30587)">30587</option>
-                        <option value="41893" :selected="(query.port == 41893)">41893</option>
-                        <option value="48574" :selected="(query.port == 48574)">48574</option>
-                        <option value="58237" :selected="(query.port == 58237)">58237</option>
+                    <select @change="selectExitCountry($event)">
+                        <option value="">All countries</option>
+                        <option v-for="server in filteredServers" :value="server.country_code" :key="server.country_code">{{ server.country }}</option>
                     </select>
                     <i></i>
                 </div>
-            </div>
-            <h4>VPN tunnel traffic</h4>
-            <div class="radio">
-                <div>
-                    <input type="radio" name="traffic_protocol" id="traffic_protocol_ipv4_ipv6" value="ipv4_ipv6" checked @change="selectTrafficProtocol($event)">
-                    <label for="traffic_protocol_ipv4_ipv6">IPv4 and IPv6</label>
+                <div class="select" v-bind:class="{ disabled: validation.exitCity }">
+                    <select :disabled="validation.exitCity" @change="selectExitCity($event)">
+                        <option value="">All cities</option>
+                        <option v-for="city in exitCities" :value="city" :key="city">{{ city }}</option>
+                    </select>
+                    <i></i>
                 </div>
-                <div>
-                    <input type="radio" name="traffic_protocol" id="traffic_protocol_ipv4" value="ipv4" @change="selectTrafficProtocol($event)">
-                    <label for="traffic_protocol_ipv4">IPv4</label>
+                <div class="select" v-bind:class="{ disabled: validation.exitServer }">
+                    <select :disabled="validation.exitServer" @change="selectExitServer($event)">
+                        <option value="">All servers</option>
+                        <option v-for="server in exitServers" :value="server.gateway + '_' + server.multihop_port" :key="server.gateway">{{ server.gateway }}</option>
+                    </select>
+                    <i></i>
                 </div>
-                <div>
-                    <input type="radio" name="traffic_protocol" id="traffic_protocol_ipv6" value="ipv6" @change="selectTrafficProtocol($event)">
-                    <label for="traffic_protocol_ipv6">IPv6</label>
+                <h3>4. Configuration</h3>
+                <h4>Multihop</h4>
+                <div class="checkbox" v-bind:class="{ disabled: validation.multihop }">
+                    <div>
+                        <input type="checkbox" id="multihop" :disabled="validation.multihop" @change="toggleMultihop($event)">
+                        <label for="multihop">Enable</label>
+                    </div>
                 </div>
-            </div>
-            <h3>5. Download</h3>
-            <a class="btn btn-big btn-border" v-bind:class="{ disabled: validation.download }" target="_blank" href="" @click.prevent="handleDownload()">Download zip archive</a>
+                <div v-if="multihop">
+                    <h4>Select entry server location</h4>
+                    <div class="select" v-bind:class="{ disabled: validation.multihop }">
+                        <select :disabled="validation.multihop" @change="selectEntryCountry($event)">
+                            <option value="">Select country</option>
+                            <option v-for="server in multihopServers" :value="server.country_code" :key="server.country_code">{{ server.country }}</option>
+                        </select>
+                        <i></i>
+                    </div>
+                    <div class="select" v-bind:class="{ disabled: validation.entryCity || validation.multihop }">
+                        <select :disabled="validation.entryCity || validation.multihop" @change="selectEntryCity($event)">
+                            <option value="">Select city</option>
+                            <option v-for="city in entryCities" :value="city" :key="city">{{ city }}</option>
+                        </select>
+                        <i></i>
+                    </div>
+                    <div class="select" v-bind:class="{ disabled: validation.entryServer || validation.multihop }">
+                        <select :disabled="validation.entryServer || validation.multihop" @change="selectEntryServer($event)">
+                            <option value="">Select server</option>
+                            <option v-for="server in entryServers" :value="server.gateway" :key="server.gateway">{{ server.gateway }}</option>
+                        </select>
+                        <i></i>
+                    </div>
+                </div>
+                <div v-if="!multihop">
+                    <h4>Port</h4>
+                    <div class="select">
+                        <select @change="selectPort($event)">
+                            <option value="2049" :selected="(query.port == 2049) || !query.port">2049</option>
+                            <option value="2050" :selected="(query.port == 2050)">2050</option>
+                            <option value="53" :selected="(query.port == 53)">53</option>
+                            <option value="1194" :selected="(query.port == 1194)">1194</option>
+                            <option value="30587" :selected="(query.port == 30587)">30587</option>
+                            <option value="41893" :selected="(query.port == 41893)">41893</option>
+                            <option value="48574" :selected="(query.port == 48574)">48574</option>
+                            <option value="58237" :selected="(query.port == 58237)">58237</option>
+                        </select>
+                        <i></i>
+                    </div>
+                </div>
+                <h4>VPN tunnel traffic</h4>
+                <div class="radio">
+                    <div>
+                        <input type="radio" name="traffic_protocol" id="traffic_protocol_ipv4_ipv6" value="ipv4_ipv6" checked @change="selectTrafficProtocol($event)">
+                        <label for="traffic_protocol_ipv4_ipv6">IPv4 and IPv6</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="traffic_protocol" id="traffic_protocol_ipv4" value="ipv4" @change="selectTrafficProtocol($event)">
+                        <label for="traffic_protocol_ipv4">IPv4</label>
+                    </div>
+                    <div>
+                        <input type="radio" name="traffic_protocol" id="traffic_protocol_ipv6" value="ipv6" @change="selectTrafficProtocol($event)">
+                        <label for="traffic_protocol_ipv6">IPv6</label>
+                    </div>
+                </div>
+                <h3>5. Download</h3>
+                <a class="btn btn-big btn-border" v-bind:class="{ disabled: validation.download }" target="_blank" href="" @click.prevent="handleDownload()">Download zip archive</a>
+            </section>
         </div>
     </div>
 </template>
