8f20d8e6c192a5053edd9451a69589b0987cc4ba
Juraj Hilje <juraj@ivpn.net>
2022-03-08 10:24:14 +0100

    Update template in SupportUA.vue

--- src/themes/ivpn-v3/assets/js/components/SupportUA.vue
+++ src/themes/ivpn-v3/assets/js/components/SupportUA.vue
@@ -4,7 +4,14 @@
             <label for="email">Email:</label>
             <input type="text" id="email" v-model="email">
             <label for="message">Message:</label>
-            <input type="text" id="message" v-model="message" />
+            <input type="text" id="message" v-model="message">
+            <div class="captcha" v-if="captchaImage">
+                <div class="image-block">
+                    <img :src="captchaImage">
+                </div>
+                <label for="login-captch">Please enter the captcha you see above:</label>
+                <input type="text" id="login-captch" v-model="captchaValue">
+            </div>
             <p v-if="error" class="error">{{ error.message }}</p>
             <button class="btn btn-big btn-solid login-btn" :disabled="inProgress || !formValid">
                 <progress-spinner v-if="inProgress" id="btn-progress" width="32" height="32" fill="#FFFFFF"/>Submit
