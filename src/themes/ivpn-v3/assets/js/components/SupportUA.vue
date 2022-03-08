08953e8f0cf46b1d352b233ca39545a7b028dcf2
Juraj Hilje <juraj@ivpn.net>
2022-03-08 10:15:18 +0100

    Update template in SupportUA.vue

--- src/themes/ivpn-v3/assets/js/components/SupportUA.vue
+++ src/themes/ivpn-v3/assets/js/components/SupportUA.vue
@@ -1,15 +1,34 @@
 <template>
-    <div></div>
+    <div>
+        <form @submit.prevent="send()">
+            <label for="email">Email:</label>
+            <input type="text" id="email" v-model="email">
+            <label for="message">Message:</label>
+            <input type="text" id="message" v-model="message" />
+            <p v-if="error" class="error">{{ error.message }}</p>
+            <button class="btn btn-big btn-solid login-btn" :disabled="inProgress || !formValid">
+                <progress-spinner v-if="inProgress" id="btn-progress" width="32" height="32" fill="#FFFFFF"/>Submit
+            </button>
+        </form>
+    </div>
 </template>
 
 <script>
+import Api from "@/api/api";
+import ProgressSpinner from "@/components/ProgressSpinner.vue";
+
 export default {
     data() {
         return {};
     },
     mounted() {},
+    computed: {
+        formValid() {
+            return this.email != "" && this.email != "";
+        },
+    },
     methods: {},
-    components: {}
+    components: { ProgressSpinner },
 };
 </script>
 
