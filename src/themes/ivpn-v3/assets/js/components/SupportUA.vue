397ed6e776d386d228cdece2b2ba0f0c052a3352
Juraj Hilje <juraj@ivpn.net>
2022-03-11 12:36:56 +0100

    Update template in SupportUA.vue

--- src/themes/ivpn-v3/assets/js/components/SupportUA.vue
+++ src/themes/ivpn-v3/assets/js/components/SupportUA.vue
@@ -1,32 +1,57 @@
 <template>
     <div>
-        <p>Welcome!</p>
-        <p>On this page, you can request a free IVPN account. This offer is available to anyone in Ukraine, Belarus and Russia. Fill in the form below and we will respond within 48 hours.</p>
+        <div v-if="lang == 'ua'">
+            <p>Welcome!</p>
+            <p>On this page, you can request a free IVPN account. This offer is available to anyone in Ukraine, Belarus and Russia. Fill in the form below and we will respond within 48 hours.</p>
+        </div>
+        <div v-if="lang == 'ru'">
+            <p>Welcome!</p>
+            <p>On this page, you can request a free IVPN account. This offer is available to anyone in Ukraine, Belarus and Russia. Fill in the form below and we will respond within 48 hours.</p>
+        </div>
         <form v-if="!messageSent" @submit.prevent="send()">
             <div class="form-input">
-                <label for="email">Email address:</label>
+                <label for="email">
+                    <span v-if="lang == 'ua'">Email address:</span>
+                    <span v-if="lang == 'ru'">Email address:</span>
+                </label>
                 <input type="email" id="email" v-model="email">
-                <label for="message">For what purpose do you need a VPN service? (brief explanation in English, Ukrainian, Russian):</label>
+                <label for="message">
+                    <span v-if="lang == 'ua'">For what purpose do you need a VPN service? (brief explanation in English, Ukrainian, Russian):</span>
+                    <span v-if="lang == 'ru'">For what purpose do you need a VPN service? (brief explanation in English, Ukrainian, Russian):</span>
+                </label>
                 <textarea id="message" v-model="message"></textarea>
                 <div class="captcha" v-if="captchaImage">
                     <div class="image-block">
                         <img :src="captchaImage">
                     </div>
-                    <label for="login-captch">Please enter the captcha you see above:</label>
+                    <label for="login-captch">
+                        <span v-if="lang == 'ua'">Please enter the captcha you see above:</span>
+                        <span v-if="lang == 'ru'">Please enter the captcha you see above:</span>
+                    </label>
                     <input type="text" id="login-captch" v-model="captchaValue">
                 </div>
             </div>
             <p v-if="error && error.message != 'Captcha Required'" class="error">{{ error.message }}</p>
             <p>
                 <button class="btn btn-big btn-solid login-btn" :disabled="!formValid">
-                    <progress-spinner v-if="inProgress" id="btn-progress" width="32" height="32" fill="#FFFFFF"/>Send request
+                    <progress-spinner v-if="inProgress" id="btn-progress" width="32" height="32" fill="#FFFFFF"/>
+                        <span v-if="lang == 'ua'">Send request</span>
+                        <span v-if="lang == 'ru'">Send request</span>
                 </button>
             </p>
         </form>
-        <p>Why do we do this? Read our and share our blog post - <a href="/blog/in-support-of-ukraine">"In support of Ukraine"</a></p>
-        <h4>About IVPN</h4>
-        <p>IVPN is an audited, open-source, privacy focused VPN service with strong security measures. We do not guarantee access to streaming apps, and our service might be inaccessible in certain areas.</p>
-        <h3 v-if="messageSent">We have received your request. Thank you!</h3>
+        <div v-if="lang == 'ua'">
+            <p>Why do we do this? Read our and share our blog post - <a href="/blog/in-support-of-ukraine">"In support of Ukraine"</a></p>
+            <h4>About IVPN</h4>
+            <p>IVPN is an audited, open-source, privacy focused VPN service with strong security measures. We do not guarantee access to streaming apps, and our service might be inaccessible in certain areas.</p>
+            <h3 v-if="messageSent">We have received your request. Thank you!</h3>
+        </div>
+        <div v-if="lang == 'ru'">
+            <p>Why do we do this? Read our and share our blog post - <a href="/blog/in-support-of-ukraine">"In support of Ukraine"</a></p>
+            <h4>About IVPN</h4>
+            <p>IVPN is an audited, open-source, privacy focused VPN service with strong security measures. We do not guarantee access to streaming apps, and our service might be inaccessible in certain areas.</p>
+            <h3 v-if="messageSent">We have received your request. Thank you!</h3>
+        </div>
     </div>
 </template>
 
