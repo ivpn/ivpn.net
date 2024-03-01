1cee7d1fab4bc6c6a2315cd9512fe622ebf59df5
Natanael Rodriguez Ramos <nathan@Natanaels-MacBook-Pro.local>
2024-03-01 12:43:39 +0000

    merge bugfix/paypal-button-not-working-on-payment-update

--- src/themes/ivpn-v3/assets/js/views/Account/AddFunds/CC.vue
+++ src/themes/ivpn-v3/assets/js/views/Account/AddFunds/CC.vue
@@ -185,10 +185,6 @@ export default {
     },
     methods: {
         async makePayment() {
-<<<<<<< HEAD
-
-=======
->>>>>>> bugfix/paypal-button-not-working-on-payment-update
             let paymentMethod = null;
             if (this.captchaPaymentMethod != null) {
                 paymentMethod = this.captchaPaymentMethod;
