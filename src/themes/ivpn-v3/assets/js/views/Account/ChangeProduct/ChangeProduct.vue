bea68eae0763a9956a81bc8869cda2099a3bbbbc
Natanael Rodriguez Ramos <nathan@Natanaels-MacBook-Pro.local>
2024-06-10 20:12:52 +0100

    Localization: QA bug fixes

--- src/themes/ivpn-v3/assets/js/views/Account/ChangeProduct/ChangeProduct.vue
+++ src/themes/ivpn-v3/assets/js/views/Account/ChangeProduct/ChangeProduct.vue
@@ -141,7 +141,7 @@ export default {
 
             this.$store.commit("setFlashMessage", {
                 type: "success",
-                message: $t('account.changeProductSuccess')  + " to " + newProductName
+                message: t('account.changeProductSuccess')  + " to " + newProductName
             });
 
             this.$router.push({ name: "account-" + this.language })
