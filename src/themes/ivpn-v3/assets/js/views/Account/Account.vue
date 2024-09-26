e4fa7b137e91bc599baa70a9d2e741b14a603443
Natanael Rodriguez Ramos <nathan@Natanaels-MacBook-Pro.local>
2024-09-26 19:47:07 +0100

    Disable product change on legacy accounts migrated

--- src/themes/ivpn-v3/assets/js/views/Account/Account.vue
+++ src/themes/ivpn-v3/assets/js/views/Account/Account.vue
@@ -134,7 +134,7 @@ export default {
             this.canChange = !product.is_locked;
         }
 
-        if(this.$store.state.auth.account.is_legacy){
+        if(this.$store.state.auth.account.is_migrated){
             this.canChange = false;
         }
     },
